634420515e8333f5235dfed38a344ba68d64f9b5
uribe-convers <uribe.convers@gmail.com>
2018-06-27 13:31:27 -0700

    CReated script to find intersect in two fasta files

---
+++ Find_Intersect_Fasta.py
@@ -0,0 +1,32 @@
+"""
+This script will return the intersect of two fasta files.
+
+Simon Uribe-Convers - December 1st, 2017 - http://simonuribe.comma
+"""
+
+import sys
+from Bio import SeqIO
+
+if __name__ == "__main__":
+    if len(sys.argv) != 4:
+        print("Usage Error, type: python "+sys.argv[0]+" 1st_fasta_file 2nd_fasta_file results_file")
+        sys.exit(0)
+
+file1 = SeqIO.parse(open(sys.argv[1]),'fasta')
+file2 = SeqIO.parse(open(sys.argv[2]), 'fasta')
+outfile = open(sys.argv[3], "w")
+
+# Create lists with the names only
+names1 = []
+for seq in file1:
+    names1.append(seq.id)
+
+names2 = []
+for seq in file2:
+    names2.append(seq.id)
+
+# Find intersect
+intersect = list(set(names1) & set(names2))
+
+out = "\r".join(intersect)
+outfile.write(out)
