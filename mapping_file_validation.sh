af0f8b23fda192ab0574b2fbd6ee16366f147b7c
uribe-convers <uribe.convers@gmail.com>
2018-08-27 10:51:05 -0700

    added step to remove empty lines

--- mapping_file_validation.sh
+++ mapping_file_validation.sh
@@ -3,6 +3,11 @@
 # This script will validate that the barcodes and sample names are unique in a mapping file. 
 # Usage: ./mapping_file_validation.sh mapping_file.tsv
 
+# Delete any empty lines if there are any
+cat $1 | sed -r 's/^\s+$//' | sed '/^$/d' > tmp
+mv tmp $1
+
+# Chack that the barcodes and sanple names are unique
 BARCODES=$(cat $1 | cut -f 1 | uniq -c | cut -d " " -f 4 | uniq)
 SAMPLES=$(cat $1 | cut -f 2 | uniq -c | cut -d " " -f 4 | uniq)
 if [[ $BARCODES == 1 && $SAMPLES == 1 ]]; then
